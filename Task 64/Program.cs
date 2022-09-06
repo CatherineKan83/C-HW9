c1df3a7ffeb4c30809f1c5f415911d691870a747
Catherine <kankatya@bk.ru>
2022-09-06 07:23:44 +0500

    added task 64

---
+++ Task 64/Program.cs
@@ -0,0 +1,8 @@
+﻿string Numbers(int c, int e)
+{
+    if(c>=e) return $" {c} " + Numbers(c-1,e);
+    else return String.Empty;
+}
+Console.Write("Введите число: ");
+int N= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("N = " + $"{N}" + "-->" + Numbers(N,1));
