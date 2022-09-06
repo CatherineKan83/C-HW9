8b043446a391b5834a91894b1c9435167593d376
Catherine <kankatya@bk.ru>
2022-09-06 08:03:00 +0500

    Added task 66

---
+++ Task 66/Program.cs
@@ -0,0 +1,11 @@
+﻿int sum(int m, int n)
+{
+    if(m>n) return (((m+n)*(m-n+1))/2);
+    else return (((m+n)*(n-m+1))/2);
+}
+
+Console.Write("Введите число M: ");
+int M= Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int N= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"M = {M}; N= {N} --> {sum(M,N)}");
